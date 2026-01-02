788a69087fa98a1c6d7f81799f474bbb7d9fb5c4
vishalsharma059 <vishalsharma02731@gmail.com>
2026-01-02 19:48:45 +0530

    change to production mode

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://assignment-workflow-portal-be.onrender.com",
+  // "http://localhost:5000/api"
+
 });
 
 api.interceptors.request.use((config) => {
